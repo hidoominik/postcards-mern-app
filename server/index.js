32b5c7497d748c838c460c908a3250056c1b09fa
hidoominik <dominik.choma.41@gmail.com>
2022-09-23 11:40:32 +0200

    env file configuration

--- server/index.js
+++ server/index.js
@@ -2,23 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
-// https://www.mongodb.com - connection to database
-const CONNECTION_URL = 'mongodb+srv://Admin:Admin@cluster0.svoh6qm.mongodb.net/?retryWrites=true&w=majority';
+// https://www.mongodb.com - database
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 
-//mongoose.set('useFindAndModify', false);
