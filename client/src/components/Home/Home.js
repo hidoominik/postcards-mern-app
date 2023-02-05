3b89fecaedb8be1875b83815e80d8a43b8ef241c
hidoominik <dominik.choma.41@gmail.com>
2023-02-05 13:52:44 +0100

    styling, add search bar

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button} from '@material-ui/core';
 import Form from '../Form/Form';
 import Posts from '../Posts/Posts';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import useStyles from './styles';
+import {useNavigate, useLocation} from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 import { getPosts } from '../../actions/posts';
 
 import Pagination from '../Pagination'
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 const Home = () => {
 
     const [currentId, setCurrentId] = useState(null); //id of current post used for updating post
     const classes = useStyles();
     const dispatch = useDispatch();
+    const query = useQuery();
+    const navigate = useNavigate();
+    const Page = query.get('page') || 1; //if there is no page use 1
+    const searchQuery = query.get('searchQuery');
+
 
     useEffect(()=>{
         dispatch(getPosts());
@@ -22,14 +33,17 @@ const Home = () => {
 
   return (
     <Grow in>
-    <Container>
+    <Container maxWidth="xl">
         <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} sm={6} md={9}>
                 <Posts setCurrentId = {setCurrentId}/>
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={6} md={3}>
+                <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                    <TextField name="search" variant="outlined" label="Search Postcard" fullWidth value="TEST" onChange={() => {}}/>
+                </AppBar>
                 <Form currentId = {currentId} setCurrentId = {setCurrentId} />
-                <Paper elevation={6}>
+                <Paper elevation={6} className={classes.pagination} >
                     <Pagination />
                 </Paper>
             </Grid>
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -23,6 +23,28 @@ export default makeStyles((theme)=>({
         marginLeft:'15px',
 
     },
+    appBarSearch:{
+        borderRadius: 15,
+        padding: '16px',
+        marginBottom: '1rem',
+        background: 'rgba( 255, 255, 255, 0.4 )',
+        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+        backdropFilter: 'blur( 5.5px )',
+        webkitBackdropFilter: 'blur( 5.5px )',
+        border: '1px solid rgba( 255, 255, 255, 0.18 )',
+        color: 'white'
+    },
+    pagination:{
+        borderRadius: 15,
+        padding: '16px',
+        marginTop: '1rem',
+        color: 'white',
+        background: 'rgba( 255, 255, 255, 0.4 )',
+        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+        backdropFilter: 'blur( 5.5px )',
+        webkitBackdropFilter: 'blur( 5.5px )',
+        border: '1px solid rgba( 255, 255, 255, 0.18 )',
+    },
     [theme.breakpoints.down('sm')]:{ //CSS for small devices
          mainContainer: {
             flexDirection:"column-reverse"
